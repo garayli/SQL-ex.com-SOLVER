765a7af416132fe1a4f0c1257da20e9c2b1f73c6
Leyla <garayli@users.noreply.github.com>
2017-10-31 00:11:19 +0300

    Delete 48(2).sql

--- SQL-ex.com-SOLVER/solutions/48(2).sql
+++
@@ -1,8 +0,0 @@
-SELECT class FROM Ships S 
-LEFT JOIN Outcomes O ON S.name = O.ship
-WHERE O.result = 'sunk'
-
-UNION
-
-SELECT class FROM Classes C LEFT JOIN Outcomes O
-ON O.ship = C.class WHERE O.result = 'sunk'
