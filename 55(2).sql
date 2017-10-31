3fa180101bc9c008c1e995b71fb5fad846f845ce
Leyla <garayli@users.noreply.github.com>
2017-11-01 00:38:38 +0300

    Create 55(2).sql
    For each class, determine the year the first ship of this class was launched. 
    If the lead ship’s year of launch is not known, get the minimum year of launch for the ships of this class.
    
    Result set: class, year.
    
     · Help topics:
        Explicit join operations
        GROUP BY clause

---
+++ 55(2).sql
@@ -0,0 +1,20 @@
+/* For each class, determine the year the first ship of this class was launched. 
+
+If the lead ship’s year of launch is not known, get the minimum year of launch for the ships of this class.
+
+Result set: class, year. */
+
+
+SELECT s.class AS class, MIN(launched) AS year
+
+FROM Ships s
+
+GROUP BY s.class
+
+UNION 
+
+SELECT c.class AS class, MIN (launched) AS year
+
+FROM Ships s FULL JOIN Classes c ON s.class = c.class
+
+GROUP BY c.class
