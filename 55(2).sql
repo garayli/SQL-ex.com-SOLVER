84b786bcb1f5ca7826d4074d27cc617fe8492ead
Leyla <garayli@users.noreply.github.com>
2017-11-01 00:39:28 +0300

    Update 55(2).sql

--- 55(2).sql
+++ 55(2).sql
@@ -1,7 +1,5 @@
 /* For each class, determine the year the first ship of this class was launched. 
-
 If the lead ship’s year of launch is not known, get the minimum year of launch for the ships of this class.
-
 Result set: class, year. */
 
 SELECT s.class AS class, MIN(launched) AS year FROM Ships s
